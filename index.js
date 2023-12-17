29e6ab180726629536c234c5bf702f30878afb2c
Aserturik <alex.hernandez27@uptc.edu.co>
2023-12-16 19:06:19 -0500

    Remove rows correctly

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const table = document.getElementById("table");
   const alert = document.getElementById("alert");
   const description = document.getElementById("description");
+  let id = 0;
+
+  function removeTodo(id) {
+    document.getElementById(id).remove();
+  }
 
   function addTodo() {
     if (title.value === "" || description.value === "") {
@@ -15,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     alert.classList.add("d-none");
 
     const row = table.insertRow();
+    row.setAttribute("id", id++);
     row.innerHTML = `
       <td>${title.value}</td>
       <td>${description.value}</td>
@@ -29,8 +35,11 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
 
     const removeBtn = document.createElement("button");
-    removeBtn.classList.add("btn","btn-danger", "mb-1", "ml-1");
-    removeBtn.innerHTML= `<i class="fa fa-trash"></i>`
+    removeBtn.classList.add("btn", "btn-danger", "mb-1", "ml-1");
+    removeBtn.innerHTML = `<i class="fa fa-trash"></i>`;
+    removeBtn.onclick = function (e) {
+      removeTodo(row.getAttribute("id"));
+    };
     row.children[3].appendChild(removeBtn);
   }
 
